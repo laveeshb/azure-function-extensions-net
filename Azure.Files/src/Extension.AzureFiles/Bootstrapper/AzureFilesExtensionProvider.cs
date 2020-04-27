26815d15947a80c059ecdc34af255c06b2ce56a8
Laveesh Bansal <laveesh.bansal@microsoft.com>
2020-04-26 21:51:01 -0700

    Add extension for Azure files

---
+++ Azure.Files/src/Extension.AzureFiles/Bootstrapper/AzureFilesExtensionProvider.cs
@@ -0,0 +1,19 @@
+﻿namespace Azure.Functions.Extension.AzureFiles
+{
+    using System;
+    using Microsoft.Azure.WebJobs.Host.Config;
+
+    public class AzureFilesExtensionProvider : IExtensionConfigProvider
+    {
+        public void Initialize(ExtensionConfigContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            var fileRule = context.AddBindingRule<AzureFileAttribute>();
+            fileRule.BindToInput(new AzureFileManager());
+        }
+    }
+}
---
+++ Azure.Files/src/Extension.AzureFiles/File/AzureFileResult.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Azure.Functions.Extension.AzureFiles
+{
+    using System.IO;
+
+    public class AzureFileResult
+    {
+        public Stream FileContentStream { get; set; }
+
+        public string SHA256Hash { get; set; }
+    }
+}
