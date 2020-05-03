0ed5bf3205108e74233ce31c39ee447e3a94775f
Laveesh Bansal <laveesh.bansal@microsoft.com>
2020-05-02 19:09:40 -0700

    Support client certificate based authentication

--- Azure.KeyVault/src/Extensions.KeyVault/Common/AuthenticationType.cs
+++ Azure.KeyVault/src/Extensions.KeyVault/Common/AuthenticationType.cs
@@ -7,5 +7,10 @@ namespace Azure.Functions.Extensions.KeyVault
         /// This type uses clientId and clientSecret to get the auth token.
         /// </summary>
         ClientSecret,
+
+        /// <summary>
+        /// This type uses clientId and clientCertificate to get the auth token.
+        /// </summary>
+        ClientCertificate,
     }
 }
--- Azure.KeyVault/src/Extensions.KeyVault/Common/KeyVaultExtensionClient.cs
+++ Azure.KeyVault/src/Extensions.KeyVault/Common/KeyVaultExtensionClient.cs
@@ -1,5 +1,7 @@
 ﻿namespace Azure.Functions.Extensions.KeyVault
 {
+    using System;
+    using System.Security.Cryptography.X509Certificates;
     using System.Threading.Tasks;
     using Microsoft.Azure.KeyVault;
     using Microsoft.IdentityModel.Clients.ActiveDirectory;
@@ -10,18 +12,46 @@
 
         public KeyVaultExtensionClient(KeyVaultPropertiesAttribute keyVaultProperties)
         {
+            if (!string.IsNullOrEmpty(keyVaultProperties.ClientSecret) && !string.IsNullOrEmpty(keyVaultProperties.EncodedClientCertificate))
+            {
+                throw new InvalidOperationException("Binding has both client secret and certificate set. Please use exactly one type of secret.");
+            }
+
             this.KeyVaultProperties = keyVaultProperties;
         }
 
         public KeyVaultClient GetKeyVaultClient()
         {
-            return new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(this.GetAccessToken));
+            if (!string.IsNullOrEmpty(this.KeyVaultProperties.ClientSecret))
+            {
+                return new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(this.GetAccessTokenWithClientSecret));
+            }
+            else if (!string.IsNullOrEmpty(this.KeyVaultProperties.EncodedClientCertificate))
+            {
+                return new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(this.GetAccessTokenWithClientCertificate));
+            }
+
+            throw new InvalidOperationException("Unexpected secret type encountered");
         }
 
-        private async Task<string> GetAccessToken(string authority, string resource, string scope)
+        private async Task<string> GetAccessTokenWithClientSecret(string authority, string resource, string scope)
         {
+            var credential = new ClientCredential(
+                clientId: this.KeyVaultProperties.ClientId,
+                clientSecret: this.KeyVaultProperties.ClientSecret);
+
             var context = new AuthenticationContext(authority);
-            var credential = new ClientCredential(clientId: this.KeyVaultProperties.ClientId, clientSecret: this.KeyVaultProperties.ClientSecret);
+            var tokenResult = await context.AcquireTokenAsync(resource, credential);
+            return tokenResult.AccessToken;
+        }
+
+        private async Task<string> GetAccessTokenWithClientCertificate(string authority, string resource, string scope)
+        {
+            var credential = new ClientAssertionCertificate(
+                clientId: this.KeyVaultProperties.ClientId,
+                certificate: new X509Certificate2(rawData: Convert.FromBase64String(this.KeyVaultProperties.EncodedClientCertificate)));
+
+            var context = new AuthenticationContext(authority);
             var tokenResult = await context.AcquireTokenAsync(resource, credential);
             return tokenResult.AccessToken;
         }
