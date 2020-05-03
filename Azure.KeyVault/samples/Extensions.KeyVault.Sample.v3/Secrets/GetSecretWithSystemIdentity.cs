065cdbc5e905862b9cfb32d71ecf25f22e696194
Laveesh Bansal <laveesh.bansal@microsoft.com>
2020-05-02 19:52:57 -0700

    add samples to get secrets from key vault using system identity

---
+++ Azure.KeyVault/samples/Extensions.KeyVault.Sample.v3/Secrets/GetSecretWithSystemIdentity.cs
@@ -0,0 +1,29 @@
+
+namespace Azure.Functions.Extensions.KeyVault.Sample.V3
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+    using Microsoft.Extensions.Logging;
+
+    public static class GetSecretWithSystemIdentity
+    {
+        [FunctionName("GetSecretWithSystemIdentity")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest request,
+            [KeyVaultSecret(
+                AuthenticationType = AuthenticationType.SystemManagedIdentity,
+                VaultName = "%VaultName%",
+                SecretName = "%TestSecretName%")] string secretValue,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var message = $"Secret value retrieved from the vault: '{secretValue}'";
+            log.LogInformation(message);
+
+            return (ActionResult)new OkObjectResult(message);
+        }
+    }
+}
