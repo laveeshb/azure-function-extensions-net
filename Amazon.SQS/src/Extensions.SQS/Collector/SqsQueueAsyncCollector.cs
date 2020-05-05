f83cab744b832fe9b79778bc50a192f084ce17c4
Juan Jacobs <juan@producteca.com>
2020-05-05 17:13:53 -0500

    Added collector

---
+++ Amazon.SQS/src/Extensions.SQS/Collector/SqsQueueAsyncCollector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Runtime;
+using Amazon.SQS;
+using Azure.Functions.Extensions.SQS.Commons;
+using Microsoft.Azure.WebJobs;
+
+namespace Azure.Functions.Extensions.SQS.Collector
+{
+	public class SqsQueueAsyncCollector : IAsyncCollector<SqsMessage>
+	{
+		private AmazonSQSClient AmazonSQSClient { get; }
+
+		public SqsQueueAsyncCollector(SqsQueueTriggerAttribute triggerParameters)
+		{
+			this.AmazonSQSClient = AmazonSQSClientFactory.Build(triggerParameters);
+
+		}
+		public Task AddAsync(SqsMessage item, CancellationToken cancellationToken = new CancellationToken())
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task FlushAsync(CancellationToken cancellationToken = new CancellationToken())
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class SqsMessage
+	{
+
+	}
+}
